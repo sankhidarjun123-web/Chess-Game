c55852401f869f732547d48b7ab6e98ac07c3a4a
Arjun Sankhi <SankhiArjun123@gmail.com>
2026-01-05 13:15:16 +0530

    Remove unused files

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-const app = require("./server");
-const PORT = 3000;
-
-const server = http.createServer(app);
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
